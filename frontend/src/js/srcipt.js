8302823113f5e99e3ca980f9eb2e9248165409e8
Gilnei Gonzo <gilbegprog@gmail.com>
2022-08-08 19:35:40 -0300

    Finished the responsive 900px e 425px

--- frontend/src/js/srcipt.js
+++ frontend/src/js/srcipt.js
@@ -1,6 +1,27 @@
-// NavBar
+// // NavBar
 
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('nav')
-  header.classList.toggle('sticky', window.scrollY > 550)
+// window.addEventListener('scroll', function () {
+//   const header = document.querySelector('header')
+//   header.classList.toggle('sticky', window.scrollY > 550)
+// })
+
+// scroll
+
+const navbar = document.querySelector('nav ul')
+const navbarButton = document.querySelector('nav button')
+const cover = document.querySelector('.cover')
+const downloadCV = document.querySelector('#download_cv')
+
+const hideNavbar = () => {
+  navbar.classList.add('hideNavbar')
+  cover.classList.add('hideCover')
+}
+
+navbarButton.addEventListener('click', () => {
+  navbar.classList.toggle('hideNavbar')
+  cover.classList.toggle('hideCover')
 })
+
+cover.addEventListener('click', hideNavbar)
+
+window.addEventListener('hashchange', hideNavbar)
